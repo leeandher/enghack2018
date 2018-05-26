c704da046eb3acbf4261ba9d8d1839915226d131
LeeAndHer <leandergrodrigues@gmail.com>
2018-05-26 14:37:23 -0400

    Basic UI Setup completed, minor functionality on the regex text input

--- hack-app/src/App.js
+++ hack-app/src/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import './App.processed.css';
+//import './marked.min.js'
+
+function Header(props) {
+  return(
+    <div className="header-container">
+      <h1 className="title">flashDash</h1>
+      <h3 className="subtitle">dynamic flash cards</h3>
+    </div>
+  )
+}
 
 function Editor(props) {
   return(
@@ -16,35 +26,70 @@ function Editor(props) {
 }
 
 function Deck(props) {
-  <div className="read-interface">
-    <div className="area">
-    <div className="subheading">{props.status}</div>
-    <ul>
-      {
-        props.deckList.map( (card, i) => {
-          <li key={`card_${i}`}>
-            <div className="term"></div>
-            <div className="definition"></div>
-          </li>
-        });
-      }
-    </ul>
-  </div>
-</div>
+  return(
+    <div className="read-interface">
+      <div className="area">
+      <div className="subheading">{props.status}</div>
+        <ul className="deck-stack">
+          {
+            props.deckList.map(
+              (card, i) => <li className="card" key={`card_${i}`}>
+                <div className="term">{card.term}</div>
+                <div className="definition">{card.definition}</div>
+              </li>
+            )
+          }
+        </ul>
+      </div>
+    </div>
+  );
 
 }
+
+class StickyNav extends Component {
+  constructor(props) {
+    super(props)
+    this.handleDownload = this.handleDownload.to
+  }
+  return(
+    <div className="function-container">
+      <button id="download" onClick={this.handleDownload}>Download Note</button>
+      <button id="review" disabled>Mark for Review</button>
+      <button id="skip" disabled>Skip</button>
+      <button id="reveal" disabled>Reveal</button>
+      <button id="review">Quiz Me!</button>
+    </div>
+  )
+}
+
+
 class Interface extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: ''
+      input: '',
+      deck: [],
     }
     this.handleInput = this.handleInput.bind(this);
+    //this.paresInputForDefinition = this.paresInputForDefinition.bind(this);
   }
 
   handleInput(e) {
     const value = e.target.value;
     this.setState({input: value});
+    let terms = this.state.input.match(/.+:/g);
+    let definitions = this.state.input.match(/:.+\s/g);
+    let newState = [];
+    if (terms && definitions && terms.length === definitions.length) {
+      terms.forEach((term, i) => {
+        let temp = {}
+        temp['term'] = term.substr(0,term.length-1);
+        temp['definition'] = definitions[i].substr(2, definitions[i].length-3);
+        newState.push(temp)
+      });
+    }
+    console.log(newState);
+    this.setState({'deck':newState});
   }
 
   render() {
@@ -52,20 +97,13 @@ class Interface extends Component {
       <div>
         <Header />
         <Editor input={this.state.input} onChange={this.handleInput} />
-        <Deck />
+        <Deck status={Object.values(this.state.deck) ? "Your deck:" :"← Start a deck!"} deckList={this.state.deck || []}/>
+        <StickyNav />
       </div>
     )
   }
 }
 
-function Header(props) {
-  return(
-    <div className="header-container">
-      <h1 className="title"><span className="part1">flash</span>Dash</h1>
-      <h3>dynamic flash cards</h3>
-    </div>
-  )
-}
 
 class App extends Component {
   render() {
